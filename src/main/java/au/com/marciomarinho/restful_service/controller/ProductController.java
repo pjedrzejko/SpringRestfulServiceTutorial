d29bdb96dfd46166eb1caca32e1741bf44ac6890
Patrick Jedrzejko <patrickjedrzejko@gmail.com>
2025-12-25 14:15:01 -0500

    changes

--- src/main/java/au/com/marciomarinho/restful_service/controller/ProductController.java
+++ src/main/java/au/com/marciomarinho/restful_service/controller/ProductController.java
@@ -1,10 +1,18 @@
 package au.com.marciomarinho.restful_service.controller;
 
 import au.com.marciomarinho.restful_service.model.Product;
+import au.com.marciomarinho.restful_service.model.ProductID;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.UUID;
+
+import static org.springframework.web.servlet.function.RequestPredicates.contentType;
 
 
 @RestController
@@ -14,10 +22,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 public class ProductController {
 
-    @PostMapping("/product")
-    public String createProduct(@RequestBody final Product product) {
+
+    @GetMapping("/product")
+    public ResponseEntity<ProductID> getProduct(@RequestBody final Product product) {
         System.out.println(product);
-        return "1234";
+        ProductID result = new ProductID(UUID.randomUUID().toString());
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(result);
+
+
 
     }
+
 }
---
+++ src/main/java/au/com/marciomarinho/restful_service/model/ProductID.java
@@ -0,0 +1,11 @@
+package au.com.marciomarinho.restful_service.model;
+
+public class ProductID {
+
+    private final String id;
+
+    public ProductID(final String id) { this.id = id; }
+
+    public String getId() { return this.id; }
+
+}
