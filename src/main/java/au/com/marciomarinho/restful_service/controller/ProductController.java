811a2e63126bb067a672041f698afb1b5739761a
Patrick Jedrzejko <patrickjedrzejko@gmail.com>
2025-12-25 13:03:23 -0500

    changes

--- src/main/java/au/com/marciomarinho/restful_service/controller/ProductController.java
+++ src/main/java/au/com/marciomarinho/restful_service/controller/ProductController.java
@@ -1,5 +1,8 @@
 package au.com.marciomarinho.restful_service.controller;
 
+import au.com.marciomarinho.restful_service.model.Product;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -11,8 +14,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 public class ProductController {
 
-    @GetMapping("/")
-    public String hello() {
-        return "Hello World";
+    @PostMapping("/product")
+    public String createProduct(@RequestBody final Product product) {
+        System.out.println(product);
+        return "1234";
+
     }
 }
